1b40f293afabbccdb5e7208b81ebef78d8544f02
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2024-01-09 23:24:13 +0530

    UI-changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./Components/Home";
 function App() {
   return (
   <>
-  <div className="bg-[#d60630]  min-h-screen bg-gradient-to-r from-maroon-500 via-maroon-600 to-maroon-700">
+  <div className=" p-[20px] text-white min-h-screen bg-gradient-to-r from-[#0575E6] to-[#021B79]">
   
   <Home/>
   
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col space-y-10 justify-center items-center'>
+    <div className='text-center'>
+        <p>Worried about Health?</p>
+        <p>No worries WE GOT THIS!!</p>
+    </div>
+  <div>
+    <input className='bg-blue-400 focus:outline-none p-5 rounded-md placeholder-white w-[300px] h-[50px]' placeholder='Enter the Product' type="text" />
+    
+  </div>
+    </div>
   )
 }
 
