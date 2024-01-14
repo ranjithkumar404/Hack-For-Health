f5def93caf8e8229f5d4153c10893f1638ec8e58
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2024-01-14 08:33:50 +0530

    new commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -23,14 +23,14 @@ const Home = () => {
     <div className='text-center text-[50px]'>
         <h1 className='head '>HEALTH BAR</h1>
         <div className='flex  items-center relative justify-center'>
-          <FaHeart className='text-xl z-30 absolute left-[90px] text-red-700' size={45}  />
+          <FaHeart className='text-xl z-30 absolute left-[95px] text-red-700' size={45}  />
           <div className='bg-white z-10  border-[3px] border-black w-[220px]  rounded-md h-[30px]'></div>
-          <div style={{width:`${progress}px`}} className='bg-red-600 rounded-l-md z-20 absolute  h-[25px] duration-300  left-[108px]'>
+          <div style={{width:`${progress}px`}} className='bg-red-600 rounded-l-md z-20 absolute  h-[25px] duration-500  left-[110px]'>
 
           </div>
           </div>
         <p className=''>Worried about Health?</p>
-        <p>No worries WE GOT THIS!!</p>
+        <p>Say no more, WE GOT THIS!!</p>
     </div>
   <div className='text-[20px]'>
     <input className='bg-blue-400 focus:outline-none p-5 rounded-md placeholder-white w-[300px]  h-[50px]' placeholder='Enter the Product' type="text" />
